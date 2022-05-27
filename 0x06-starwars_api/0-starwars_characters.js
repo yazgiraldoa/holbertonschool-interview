ce4ea7eb8a7b4a471d941fb41ce19d242fdd7474
yazgiraldoa <edygiraldo.97@gmail.com>
2022-05-26 20:54:42 -0500

    add starwars api project

---
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+// Script that prints all characters of a Star Wars movie
+
+const request = require('request');
+const { argv } = require('process');
+const url = 'https://swapi-api.hbtn.io/api/films/' + argv[2];
+
+request.get(url, function (error, response, body) {
+  if (error) {
+    console.log(error);
+    return;
+  }
+  const json = JSON.parse(body);
+  const arr = json.characters;
+  arr.forEach(async function (person) {
+    try {
+      const result = await makeRequest(person);
+      const dic = JSON.parse(result)
+      console.log(dic.name);
+    } catch (error) {
+      console.error(error);
+    }
+  })
+});
+
+
+function makeRequest(path) {
+    return new Promise(function (resolve, reject) {
+        request.get(path, function (error, response, body) {
+            if (error) {
+              reject(error);
+            }
+            resolve(body)
+        });
+    });
+}
---
+++ 0x06-starwars_api/README.md
@@ -0,0 +1,12 @@
+<h1 class="gap">0x06. Star Wars API</h1>
+
+<h2>Task #0</h2>
+
+<p>Write a script that prints all characters of a Star Wars movie:</p>
+
+<ul>
+<li>The first positional argument passed is the Movie ID - example: <code>3</code> = &ldquo;Return of the Jedi&rdquo; </li>
+<li>Display one character name per line <strong>in the same order as the &ldquo;characters&rdquo; list in the <code>/films/</code> endpoint</strong></li>
+<li>You must use the <a href="/rltoken/gOFjTqtp8L2xueAR74gqUw" title="Star wars API" target="_blank">Star wars API</a></li>
+<li>You must use the <code>request</code> module</li>
+</ul>
